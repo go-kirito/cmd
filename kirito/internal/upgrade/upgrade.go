ac44f08bd69f45bbe278e8827c8b55d0ba655862
nopsky <cnnopsky@gmail.com>
2022-08-16 13:53:45 +0800

    fix:install cmd error

--- kirito/internal/upgrade/upgrade.go
+++ kirito/internal/upgrade/upgrade.go
@@ -18,11 +18,11 @@ var CmdUpgrade = &cobra.Command{
 
 // Run upgrade the kratos tools.
 func Run(cmd *cobra.Command, args []string) {
-	err := base.GoGet(
-		"github.com/go-kirito/cmd/kirito",
-		"github.com/go-kirito/cmd/protoc-gen-go-kirito",
-		"google.golang.org/protobuf/cmd/protoc-gen-go",
-		"github.com/envoyproxy/protoc-gen-validate",
+	err := base.GoInstall(
+		"github.com/go-kirito/cmd/kirito@latest",
+		"github.com/go-kirito/cmd/protoc-gen-go-kirito@latest",
+		"google.golang.org/protobuf/cmd/protoc-gen-go@latest",
+		"github.com/envoyproxy/protoc-gen-validate@latest",
 	)
 	if err != nil {
 		fmt.Println(err)
